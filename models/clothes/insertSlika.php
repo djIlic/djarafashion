53cdfc5d43ad0474b0b502b73387fdea28b1df5c
djIlic <dorde.ilic97@gmail.com>
2019-06-15 16:20:51 +0200

    Novi fajlovi

--- models/clothes/insertSlika.php
+++ models/clothes/insertSlika.php
@@ -7,19 +7,12 @@ if(isset($_POST['btnSlika'])){
 
     $artiklId=$_POST['ddlArtikl'];
 
-    // FAJL
-
-    // $fajl = $_FILES['slika'];
-    // var_dump($fajl);
 
     $fajl_naziv = $_FILES['slika']['name'];
     $fajl_tmpLokacija = $_FILES['slika']['tmp_name'];
     $fajl_tip = $_FILES['slika']['type'];
     $fajl_velicina = $_FILES['slika']['size'];
 
-    // VALIDACIJA
-
-    // 3 000 000 ~ 3MB
     $greske = [];
 
     $dozvoljeni_tipovi = ['image/jpg', 'image/jpeg', 'image/png'];
@@ -33,21 +26,9 @@ if(isset($_POST['btnSlika'])){
 
     
     if(count($greske) == 0){
-        // var_dump(getimagesize($fajl_tmpLokacija));
-
-        // $dimenzije = getimagesize($fajl_tmpLokacija);
-        // $sirina = $dimenzije[0];
-        // $visina = $dimenzije[1];
 
         list($sirina, $visina) = getimagesize($fajl_tmpLokacija);
 
-        // KREIRANJE PRAZNE SLIKE
-        
-        // $slika = imagecreatetruecolor(50, 50);
-        // imagejpeg($slika, '../../assets/images/users/prazna_slika.jpg');
-        
-        // KREIRANJE SLIKE OD FAJLA
-        
         $postojecaSlika = null;
         switch($fajl_tip){
             case 'image/jpeg':
@@ -58,28 +39,18 @@ if(isset($_POST['btnSlika'])){
                 break;
         }
 
-        // PRIMER 1 - definisanje nove velicine slike
-        // $novaSirina = 200;
-        // $novaVisina = 200;
-
-        // PRIMER 2 - procentualno smanjenje - za 50% manje sirina i visina
-        // $novaSirina = $sirina * 0.5;
-        // $novaVisina = $visina * 0.5;
-        
-        // PRIMER 3 - srazmerno smanjenje - sirina: 200px, visina: ?
+  
         $novaSirina = 245;
-        $novaVisina = ($novaSirina/$sirina) * $visina; // novaVisina : visina = novaSirina : sirina
+        $novaVisina = ($novaSirina/$sirina) * $visina; 
 
-        // kreiranje prazne slike
+        
         $novaSlika = imagecreatetruecolor($novaSirina, $novaVisina);
         
-        // RESIZE
-
-        // po referenci: u $novaSlika ce se nalaziti smanjena upload-ovana slika
+        
         imagecopyresampled($novaSlika, $postojecaSlika, 0, 0, 0, 0, $novaSirina, $novaVisina, $sirina, $visina);
 
 
-        // UPLOAD NOVE SLIKE
+        
         $naziv = time().$fajl_naziv;
         $putanjaNovaSlika = 'assets/images/nova_'.$naziv;
 
@@ -110,7 +81,7 @@ if(isset($_POST['btnSlika'])){
             }
         }
 
-        // brisanje iz memorije
+        
         imagedestroy($postojecaSlika);
         imagedestroy($novaSlika);
 
